333296ce12581cc4a287170da1ce581a357a32e8
minhwuan1234 <100684479+minhwuan1234@users.noreply.github.com>
2026-07-20 11:48:00 +0700

    Update config.js

--- frontend/config.js
+++ frontend/config.js
@@ -1,4 +1,4 @@
 window.APP_CONFIG = {
   supabaseUrl: "https:///wtneevzemprtgsdaifew.supabase.co",
-  supabasePublishableKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind0bmVldnplbXBydGdzZGFpZmV3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQyNDU2NzMsImV4cCI6MjA5OTgyMTY3M30.2LGr0Nn7aj6PHgVmj20otGsaV5K9b0maoo2iQj9-3bk"
+  supabasePublishableKey: "sb_publishable_W4M7pFMTxRiTA376qwBXVA_TroZyrx2"
 };
