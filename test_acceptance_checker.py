49574fa9ad44aa6a806cec13c440b9fe873636a9
minhwuan1234 <100684479+minhwuan1234@users.noreply.github.com>
2026-08-16 13:36:35 +0700

    Update test_acceptance_checker.py

--- test_acceptance_checker.py
+++ test_acceptance_checker.py
@@ -6,7 +6,7 @@ from app.outreach_account_pool import (
 )
 
 
-ACCOUNT_ID = "outreach_account_2"
+ACCOUNT_ID = "outreach_account_02"
 LINKEDIN_URL = "https://www.linkedin.com/in/minh-quân-851170229/"
 
 
