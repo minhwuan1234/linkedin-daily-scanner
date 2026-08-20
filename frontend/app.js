a4a499198a895d798afff35f3110f150e636d789
minhwuan1234 <100684479+minhwuan1234@users.noreply.github.com>
2026-08-20 17:06:23 +0700

    Update app.js

--- frontend/app.js
+++ frontend/app.js
@@ -4181,7 +4181,7 @@ function openMessageSendModal(
 
   if (els.messageTemplateInput) {
     els.messageTemplateInput.value =
-      "Hi {first_name},\n\n";
+      "Hi {first_name},\n\nI’ve been seeing a bunch of agencies adding motion/animation into client campaigns lately. Out of curiosity, is that something you are exploring too, or do you guys prefer to keep it simple? Would love to hear your thoughts!";
   }
 
   if (els.messageSendError) {
